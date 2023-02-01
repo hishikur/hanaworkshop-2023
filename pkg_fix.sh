5cd1be67a1ef741c5b99d5b16338385a524fc64f
hishikur <35244573+hishikur@users.noreply.github.com>
2023-02-01 13:23:49 +0900

    master

--- pkg_fix.sh
+++ pkg_fix.sh
@@ -9,5 +9,5 @@ sudo yum clean all
 sudo rm -rf /var/cache/yum/*
 sudo rm -rf /var/cache/dnf
 sudo yum remove katello-ca-consumer-labsat.opentlc.com -y
-##sudo subscription-manager register --org=7728826 --activationkey=37fc
 sudo subscription-manager register --org=11594663 --activationkey=tempkey
+subscription-manager release --set=8.4
